b9be5508c862f301bfdc172075905ae23816750c
Amine Lamuadni <W7equwE5@gmail.com>
2023-08-18 16:06:40 -0700

    Add shell.h file

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char **environ;
+
+#endif /* SHELL_H */
