f04cb74486c9b5f4291e871ea76cd8e4a7c49626
Amine Lamuadni <W7equwE5@gmail.com>
2023-08-23 07:33:09 -0700

    Update shell.c file

--- shell.c
+++ shell.c
@@ -66,15 +66,13 @@ int main(int argc, char *argv[])
 
 	while (1)
 	{
-		if (is_interactive())
-			write(STDOUT_FILENO, "#cisfun$ ", 9);
+		write(STDOUT_FILENO, "#cisfun$ ", 9);
 
 		nread = read_input(&line, &len);
 
 		if (nread == -1)
 		{
-			if (is_interactive())
-				write(STDOUT_FILENO, "\n", 1);
+			write(STDOUT_FILENO, "\n", 1);
 
 			free(line);
 			exit(EXIT_SUCCESS);
