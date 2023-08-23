763c647bf58f7826f3c65ae18837f2f97b658a07
moncef63 <moncif.ayouch@gmail.com>
2023-08-23 16:33:58 -0700

    Add is_interactive.c

--- is_interactive.c
+++ is_interactive.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * is_interactive - Checks if the shell is running in interactive mode.
- * Return: 1 if interactive, 0 otherwise.
+ * is_interactive - Checks if the simple shell is running in interactive mode.
+ * Return: 1 if interactive, 0 if not.
  */
 int is_interactive(void)
 {
