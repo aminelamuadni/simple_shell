2d82b89c55a05431a90fb5fe02fcef28c172e53e
Amine Lamuadni <W7equwE5@gmail.com>
2023-08-23 09:09:07 -0700

    Update shell.h file

--- shell.h
+++ shell.h
@@ -7,10 +7,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct shell_data - Data structure for the shell.
+ * @line: The input line read from stdin.
+ */
+typedef struct shell_data
+{
+	char *line;
+} shell_data_t;
+
 extern char **environ;
 
+shell_data_t *get_data(void);
+void handle_sigint(int sig);
 ssize_t read_input(char **line, size_t *len);
-void execute_command(char *cmd, char **args, char *argv[], char *line);
+void execute_command(char *cmd, char **args, char *argv[]);
 int is_interactive(void);
 
 #endif /* SHELL_H */
