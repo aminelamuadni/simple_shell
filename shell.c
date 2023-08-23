16da5a3286a9c9f3682a32d46102518885a59053
Amine Lamuadni <W7equwE5@gmail.com>
2023-08-23 14:22:36 -0700

    Update shell.c file

--- shell.c
+++ shell.c
@@ -1,67 +1,6 @@
 #include "shell.h"
 
 /**
- * get_data - Returns a pointer to the shell's data structure.
- *
- * Return: Pointer to the static instance of shell_data_t.
- */
-shell_data_t *get_data(void)
-{
-	static shell_data_t data = {NULL};
-
-	return (&data);
-}
-
-/**
- * read_input - Reads a line from stdin.
- * @line: Pointer to the line buffer.
- * @len: Length of the buffer.
- * Return: Number of characters read.
- */
-ssize_t read_input(char **line, size_t *len)
-{
-	return (getline(line, len, stdin));
-}
-
-/**
- * execute_command - Executes a given command.
- * @cmd: The command to execute.
- * @args: The arguments for the command.
- * @argv: Argument vector from main for error reporting.
- */
-void execute_command(char *cmd, char **args, char *argv[])
-{
-	pid_t child_pid;
-	int status;
-	shell_data_t *data = get_data();
-
-	child_pid = fork();
-
-	if (child_pid == 0)
-	{
-		if (execve(cmd, args, environ) == -1)
-		{
-			perror(argv[0]);
-			free(data->line);
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		wait(&status);
-	}
-}
-
-/**
- * is_interactive - Checks if the shell is running in interactive mode.
- * Return: 1 if interactive, 0 otherwise.
- */
-int is_interactive(void)
-{
-	return (isatty(STDIN_FILENO));
-}
-
-/**
  * main - Entry point for simple shell.
  * @argc: Argument count (not used).
  * @argv: Argument vector, used for error reporting.
@@ -72,7 +11,7 @@ int main(int argc, char *argv[])
 	shell_data_t *data = get_data();
 	size_t len = 0;
 	ssize_t nread;
-	char *args[2];
+	char **args;
 
 	(void) argc;
 
@@ -80,8 +19,7 @@ int main(int argc, char *argv[])
 
 	while (1)
 	{
-		if (is_interactive())
-			write(STDOUT_FILENO, "$ ", 2);
+		print_prompt();
 
 		nread = read_input(&data->line, &len);
 
@@ -89,16 +27,15 @@ int main(int argc, char *argv[])
 		{
 			if (is_interactive())
 				write(STDOUT_FILENO, "\n", 1);
-
 			free(data->line);
 			exit(EXIT_SUCCESS);
 		}
 
 		data->line[nread - 1] = '\0';
-		args[0] = data->line;
-		args[1] = NULL;
 
-		if (data->line[0])
+		args = tokenize_input(data->line);
+
+		if (args[0] && args[0][0])
 			execute_command(args[0], args, argv);
 
 		free(data->line);
