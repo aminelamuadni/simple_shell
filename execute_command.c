8ff09ca7a186c7af6b8b0141c0cd6d9739b92eb2
moncef63 <moncif.ayouch@gmail.com>
2023-08-23 18:02:00 -0700

    restoring execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -1,54 +1,29 @@
-#include "shell.h"
-
 /**
  * execute_command - Executes a given command.
+ * @cmd: The command to execute.
+ * @args: The arguments for the command.
+ * @argv: Argument vector from main for error reporting.
  * @cmd: command to execute.
  * @args: arguments for the commands.
  * @argv: Argument vector from the main for errors reporting.
  */
 void execute_command(char *cmd, char **args, char *argv[])
 {
-    shell_data_t *data = get_data();
-
- 
-    char *path = getenv("PATH");
-    char *path_copy = strdup(path);
-    char *dir = strtok(path_copy, ":");
-
-    while (dir != NULL)
-    {
-        char full_path[1024];
-        snprintf(full_path, sizeof(full_path), "%s/%s", dir, cmd);
-
- 
-        if (access(full_path, X_OK) == 0)
-        {
- 
-            pid_t child_pid = fork();
-
-            if (child_pid == 0)
-            {
-                execve(full_path, args, environ);
-                perror(argv[0]);
-                free(data->line);
-                free(path_copy);
-                exit(EXIT_FAILURE);
-            }
-            else
-            {
-                wait(NULL);
-                free(path_copy);
-                return;
-            }
-        }
-
-        dir = strtok(NULL, ":");
-    }
-
-    // Command not found, print an error message
-    fprintf(stderr, "%s: command not found\n", cmd);
-
-    // Free memory allocated
-    free(path_copy);
+	pid_t child_pid;
+	int status;
+	shell_data_t *data = get_data();
+	child_pid = fork();
+	if (child_pid == 0)
+	{
+		if (execve(cmd, args, environ) == -1)
+		{
+			perror(argv[0]);
+			free(data->line);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		wait(&status);
+	}
 }
-
